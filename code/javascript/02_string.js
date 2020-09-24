8e30699af8943076553d2b368a39edd43cfad480
galocruzc <galocruz20@gmail.com>
2020-09-23 21:05:29 -0500

    primera subida de datos 23092020

---
+++ code/javascript/02_string.js
@@ -0,0 +1,12 @@
+var apellido = 'Cruz'
+var nombre= 'Galo' , apellido 'cruz'
+
+var nombreMayusculas = nombre.toUpperCase()
+
+var apellidoMayusculas = apellido.toUpperCase()
+
+console.log(nombreMayusculas + '' + apellidoMayusculas)
+
+console.log ( '${nombre} ${apellidoMasyusculas}' )
+ var longitudNombre = nombre.length
+ 
