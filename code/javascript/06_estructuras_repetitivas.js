84882b645a77d52fa34c67b9f8dcc916c71b444f
galocruzc <galocruz20@gmail.com>
2020-10-01 19:25:08 -0500

    Estructuras Repetitivas.

---
+++ code/javascript/06_estructuras_repetitivas.js
@@ -0,0 +1,25 @@
+const aumentarPeso = ( objeto ) => objeto.peso += INCREMENTAR_PESO
+const disminuyePeso = ( objeto ) => objeto.peso -= DECREMENTAR_PESO
+
+const comeMucho = () => Math.random() < 0.1
+const realizaDeporte = () => Math.random() < 0.8
+const comeMucho = () => Math.random() < 0.7
+const realizaDeporte = () => Math.random() < 0.2
+
+const META = persona.peso - 10
+
+console.log( `Al inicio del año ${persona.nombre} pesa ${persona.peso}.` )
+
+while( persona.peso > META ) {
+    if (comeMucho()) {
+        console.log('Aumenta el peso.')
+        aumentarPeso( persona )
+    }
+    if (realizaDeporte()) {
+        console.log('Disminuye el peso.')
+        disminuyePeso( persona )
+    }
+}
+
+console.log( `Al final del año ${persona.nombre} pesa ${persona.peso}.` ) 
+console.log( `Al final del año ${persona.nombre} pesa ${persona.peso}.` )
